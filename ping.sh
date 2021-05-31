5cc5fabbe9df4e7c546506c89ebaa6e4d0b26bce
lemo96 <85106586+lemo96@users.noreply.github.com>
2021-05-31 14:41:29 +0430

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-var1=$1
+#! /bin/bash
+
+read -n 9 -p "Please type your ip Range etc //195.114.8// >> " var1
+
+echo "Your Ip range is $var1"
 
 is_alive_ping()
 {
   ping -c 1 $1 > /dev/null
-  [ $? -eq 0 ] && echo "$var1$i"
+  [ $? -eq 0 ] && echo "$var1.$i"
 }
 a=44
 
@@ -12,7 +15,7 @@ for i in $(seq 1 254)
 
 do
 
-is_alive_ping $var1$i & disown
+is_alive_ping $var1.$i & disown
 
 
 pkill disown
